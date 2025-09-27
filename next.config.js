ec3c944c597604c795ef250e4a0693c1c32ef0fc
김유나 <yuna000804@gmail.com>
2025-09-28 01:07:53 +0900

    chore: configure Next.js static export and add GitHub Pages deploy workflow

--- next.config.js
+++ next.config.js
@@ -1,13 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   webpack: (config) => {
+//     config.module.rules.push({
+//       test: /\.svg$/i,
+//       issuer: /\.[jt]sx?$/,
+//       use: ["@svgr/webpack"],
+//     });
+//     return config;
+//   },
+// };
 
+// module.exports = nextConfig;
+
+// next.config.js
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const repo = "team_biskit"; // 레포 이름
+
+const nextConfig = {
+  output: "export", // 정적 내보내기 활성화
+  images: { unoptimized: true }, // GH Pages에서 이미지 최적화 비활성화
+  trailingSlash: true, // 404 이슈 방지
+  basePath: isProd ? `/${repo}` : "",
+  assetPrefix: isProd ? `/${repo}/` : undefined,
+};
 module.exports = nextConfig;
